3c3dcec364ddb42e48e423008feae9e8e070a0ca
Kovshun-Olena <kovshun.elena.1973@gmail.com>
2025-05-01 21:38:44 +0300

    promise

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,16 +1,51 @@
 console.log("Snackbar");
 
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 
-// const form = document.querySelector(".form");
+const form = document.querySelector(".form");
 
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-//     const promiseState = event.target.elements.state, value;
-//     const delay = event.target.elements.delay.value;
-//     console.log(delay);
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  if (isNaN(delay) || delay <= 0) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a valid delay greater than 0',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {      
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  })
     
-// })
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+});
